6b8ef464d5ad794112457893e0966770d2a71c41
supersimplenet <supersimplenet@gmail.com>
2020-02-16 16:56:21 +0100

    Add codepen css & js files

---
+++ assets/js/codepen.js
@@ -0,0 +1,10 @@
+const el = (domstring) => {
+    const html = new DOMParser().parseFromString(domstring, 'text/html');
+    return html.body.firstChild;
+};
+
+let watermark = `<a class="watermark" href="https://super-simple.net">
+                     <img src="https://s.cdpn.io/profiles/user/2463031/80.jpg?1581097114">
+                 </a>`;
+
+document.body.append(el(watermark));
